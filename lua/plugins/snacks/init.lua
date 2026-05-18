eea52ceb50472006c53750fb84c240eff4fd5145
Olivier Bitter <olibitter@gmail.com>
2026-05-18 22:28:50 +0200

    minimal setup

---
+++ lua/plugins/snacks/init.lua
@@ -0,0 +1,43 @@
+return {
+  "snacks.nvim",
+  lazy = false,
+  priority = 1000,
+  after = function(plugin)
+    require('snacks').setup({
+      picker = {
+        sources = {
+          explorer = {
+            auto_close = false,
+          },
+        },
+      },
+      indent = {
+        scope = {
+          hl = 'MySnacksIndent',
+        },
+        chunk = {
+          -- enabled = true,
+          hl = 'MySnacksIndent',
+        }
+      },
+      statuscolumn = {
+        enabled = false,
+        left = { "mark", "git" },   -- priority of signs on the left (high to low)
+        right = { "sign", "fold" }, -- priority of signs on the right (high to low)
+        folds = {
+          open = false,             -- show open fold icons
+          git_hl = false,           -- use Git Signs hl for fold icons
+        },
+        git = {
+          -- patterns to match Git signs
+          patterns = { "GitSign", "MiniDiffSign" },
+        },
+        refresh = 50, -- refresh at most every 50ms
+      },
+    })
+    -- Handle the backend of those remote commands.
+    -- hopefully this can be removed one day
+    -- NOTE: we aren't loading this lazily, and the keybinds already are so it is fine to just set these here
+    require("plugins.snacks.keymaps")
+  end
+}
---
+++ lua/plugins/undotree.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "nvim.undotree",
+    cmd = {
+      "Undotree",
+    },
+    keys = {
+      {
+        "<leader>U",
+        "<cmd>Undotree<cr>",
+        mode = { "n" },
+        desc = "Undo Tree"
+      },
+    },
+  },
+}
