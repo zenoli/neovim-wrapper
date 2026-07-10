2d1eb6e341dd35fd966a7e1b0cd9fbdf1b422744
Olivier Bitter <olibitter@gmail.com>
2026-07-10 23:48:20 +0200

    wip

---
+++ lua/lang/python/test.lua
@@ -0,0 +1,3 @@
+return function(name)
+  require("neotest-" .. name)
+end
--- lua/plugins/neotest/init.lua
+++ lua/plugins/neotest/init.lua
@@ -1,5 +1,17 @@
-return {
-  { "neotest-python", dep_of = "neotest" },
+local get_neotest_extensions = function()
+  local extensions = {}
+  local lang_dir = vim.fn.stdpath("config") .. "/lua/lang"
+  for name, type in vim.fs.dir(lang_dir) do
+    if type == "directory" then
+      if vim.uv.fs_stat(lang_dir .. "/" .. name .. "/test.lua") then
+        table.insert(extensions, { "neotest-" .. name, dep_of = "neotest" })
+      end
+    end
+  end
+  return extensions
+end
+
+return vim.list_extend(get_neotest_extensions(), {
   {
     "neotest",
     keys = require("plugins.neotest.keymaps"),
@@ -11,4 +23,4 @@ return {
       })
     end,
   },
-}
+})
