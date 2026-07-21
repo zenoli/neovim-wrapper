19eb214abdfeab5d43f9b42dff00f02180685907
Olivier Bitter <olibitter@gmail.com>
2026-07-21 16:59:14 +0200

    markview: enable hybrid_modes

--- lua/lang/config/markdown/plugins/markview-nvim.lua
+++ lua/lang/config/markdown/plugins/markview-nvim.lua
@@ -1,4 +1,13 @@
+---@type lze.PluginSpec
 return {
   "markview.nvim",
   lazy = false,
+  after = function(_)
+    require("markview").setup({
+      preview = {
+        linewise_hybrid_mode = false,
+        hybrid_modes = { "n" },
+      },
+    })
+  end,
 }
