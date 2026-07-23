98f5bc4846e045a15089ea6adf5b01cd0f8390db
Olivier Bitter <olibitter@gmail.com>
2026-07-23 15:58:15 +0200

    add inlay-hint capability

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -11,12 +11,17 @@ vim.diagnostic.config({
   },
 })
 
-vim.api.nvim_create_autocmd('LspAttach', {
+vim.api.nvim_create_autocmd("LspAttach", {
   callback = function(ev)
     local bufnr = ev.buf
+    local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
     require("lsp.keymaps").load(bufnr)
-    vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+    vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
       vim.lsp.buf.format()
-    end, { desc = 'Format current buffer with LSP' })
+    end, { desc = "Format current buffer with LSP" })
+
+    if client:supports_method("textDocument/inlayHint") then
+      vim.lsp.inlay_hint.enable(true, { bufnr = bufnr })
+    end
   end,
 })
